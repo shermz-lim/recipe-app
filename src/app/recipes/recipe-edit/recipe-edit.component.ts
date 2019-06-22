0ce32be7724640ee40c7633e6755a6175381a2d7
shermzlim <shermz.lim.jh@gmail.com>
2019-06-22 17:18:02 +0800

    added base form for recipe edit/new

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -11,6 +12,7 @@ import { RecipeService } from '../recipe.service';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeEditForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService) { }
@@ -20,8 +22,23 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = +params.id;
         this.editMode = params.id != null;
+        this.initForm();
       }
     );
+
+  }
+
+  private initForm() {
+    this.recipeEditForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      imagePath: new FormControl(null, [Validators.required]),
+      description: new FormControl(null, [Validators.required])
+    });
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      this.recipeEditForm.setValue(recipe);
+    }
   }
 
 }
